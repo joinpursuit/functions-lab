2a6e8361f5ff1175498f364b03c70794bb4f4f41
Ben Stone <benjamin.h.stone@gmail.com>
2020-09-17 17:19:53 -0400

    add sum file

---
+++ problems/sum.js
@@ -0,0 +1,13 @@
+/**
+ * Takes in two values and returns their sum
+
+ * @param {number} a - The first number to add.
+ * @param {number} b - The second number to add.
+ * @returns {number} Returns a number equal to the sum of a and b
+ */
+
+function sum() {
+
+}
+
+module.exports = sum;
