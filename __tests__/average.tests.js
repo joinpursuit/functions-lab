0c3d8583a9a4be32a6ead04f23f90cac06bb5951
Ben Stone <benjamin.h.stone@gmail.com>
2020-09-18 16:18:35 -0400

    update tests name

---
+++ __tests__/average.tests.js
@@ -0,0 +1,5 @@
+const average = require("./../problems/average.js")
+
+test('averages 4 and 10 to equal 7', () => {
+  expect(average(4,10)).toBe(7)
+})
---
+++ problems/average.js
@@ -0,0 +1,13 @@
+/**
+ * Takes in two values and returns their average
+
+ * @param {number} a - The first number
+ * @param {number} b - The second number
+ * @returns {number} Returns a number equal to the average of a and b
+ */
+
+function average() {
+
+}
+
+module.exports = average;
