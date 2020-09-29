4df18cbdf2bb693f2f3bebe1c8daf02d67cfbf11
James Byess <james.byess@gmail.com>
2020-09-29 14:40:36 -0400

    sum tests

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -21,31 +21,34 @@ describe("2. sum", () => {
 
 describe("2. sumThree", () => {
   test("does not return undefined", () => {
-
+    expect(sumThree(1,2,3)).toBeDefined()
+    expect(sumThree(9,9,9)).toBeDefined()
   })
 
   test("returns a number", () => {
-
+    expect(typeof sumThree(100, 5, 1)).toBe('number')
+    expect(typeof sumThree(1, 1, 1)).toBe('number')
   })
 
   test('returns the correct answer', () => {
-
+    expect(sumThree(9,9,9)).toBe(27)
+    expect(sumThree(8, 1, 2)).toBe(11)
   })
-
 })
 
 describe("2. subtract", () => {
   test("does not return undefined", () => {
-
+    expect(subtract(2, 1)).toBeDefined()
+    expect(subtract(100, 100)).toBeDefined()
   })
   
   test("returns a number", () => {
-
+    expect(typeof subtract(100, 100)).toBe('number')
+    expect(typeof subtract(99, 10)).toBe('number')
   })
 
   test('returns the correct answer', () => {
-
+    expect(subtract(2, 1)).toBe(1)
+    expect(subtract(1024, 512)).toBe(512)
   })
-
-
 })
