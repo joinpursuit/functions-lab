0558919ec8bb66cb07d8338d90da5f8cbf5de3dc
joshuamendez <joshuafmendez@gmail.com>
2020-12-08 20:22:06 -0500

    completed types.js file

--- problems/8.types.js
+++ problems/8.types.js
@@ -17,8 +17,8 @@
  * @param {number} b - the second number
  * @returns {boolean} - if a and b are both number types, return true
  */
-function bothNumbers() {
-
+function bothNumbers( a, b) {
+  return typeof a === 'number' && typeof b === 'number';
 }
 
 /**
@@ -29,8 +29,8 @@ function bothNumbers() {
  * @returns {boolean} - return true if either a or b are a number
  * 
  */
-function eitherNumber() {
-
+function eitherNumber(a, b) {
+  return typeof a === 'number' || typeof b === 'number';
 }
 
 /**
@@ -40,8 +40,8 @@ function eitherNumber() {
  * @param {number} b - The second number to test
  * @returns {boolean} - If a and b are equal then return true, otherwise return false
  */
-function numbersEqual() {
-
+function numbersEqual(a, b) {
+  return a === b;
 }
 
 /**
@@ -51,8 +51,8 @@ function numbersEqual() {
  * @param {number} b - The second number to test
  * @returns {boolean} - If a and b are not equal then return true, otherwise return false
  */
-function numbersNotEqual() {
-
+function numbersNotEqual(a, b) {
+  return !(a === b);
 }
 
 /**
@@ -63,8 +63,8 @@ function numbersNotEqual() {
  * @param {number} b - The second number to test
  * @returns {boolean} - If a and b are both even numbers, return true
  */
-function bothEven() {
-
+function bothEven(a, b) {
+  return !(a % 2) && !(b % 2);
 }
 
 /**
@@ -74,8 +74,8 @@ function bothEven() {
  * @param {number} b - The second number to test
  * @returns {boolean} - If a and b are both odd numbers, return true
  */
-function bothOdd() {
-  
+function bothOdd(a, b) {
+  return !!(a % 2) && !!(b % 2);;
 }
 
 /**
@@ -85,8 +85,8 @@ function bothOdd() {
  * @param {number} b - The second number to test
  * @returns {boolean} - If either a or b is even, return true
  */
-function eitherEven() {
-
+function eitherEven(a, b) {
+  return !(a % 2) || !(b % 2);
 }
 
 module.exports = {
