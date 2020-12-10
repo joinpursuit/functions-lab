648cf97b9968c4d69f76d8bb16fb06abad0decc0
Kathy Gee <mzchick@gmail.com>
2020-12-09 22:10:57 -0500

    finish problems

--- problems/7.defaults.js
+++ problems/7.defaults.js
@@ -5,9 +5,11 @@
  * @param {number} [5] n - The number to be returned. Default value is 5.
  * @returns {number} Equal to either 5 or n
  */
-function defaultNum() {
-
+function defaultNum(n) {
+  return n || 5
+  
 }
+defaultNum()
 
 /**
  * Returns whatever string is passed in, or "hello world" if none is passed in
@@ -15,7 +17,8 @@ function defaultNum() {
  * @param {string} ["hello world"] str - The string to be returned
  * @returns {string} - Equal to str, or "hello world"
  */
-function defaultStr() {
+function defaultStr(str) {
+  return str || "hello world"
 
 }
 
