87d052e10ddddb85eddf9079e93ec6e8e3f03765
andres ballares <andr3sb@hotmail.com>
2020-12-12 15:23:40 -0500

    resolved problems

--- problems/1.intro.js
+++ problems/1.intro.js
@@ -4,6 +4,8 @@
  * @returns undefined
  */
 function helloWorld() {
+    console.log("hello world")
+
 
 }
 
@@ -14,7 +16,8 @@ function helloWorld() {
  * @param {string} name - the name to be added to the console log 
  * @returns undefined
  */
-function helloName() {
+function helloName(name) { 
+  console.log("hello " + name)
 
 }
 
@@ -23,7 +26,9 @@ function helloName() {
  * 
  * @returns {string} - the string "hello world"
  */
-function returnHelloWorld() {
+function returnHelloWorld(helloWorld) { 
+    return 'hello world'
+
 
 }
 
@@ -34,7 +39,9 @@ function returnHelloWorld() {
  * @param {string} name - the name to added to the return string
  * @returns {string} A greeting in the form of "Hello ${name}!"
  */
-function returnHelloName() {
+function returnHelloName(name) {
+  return "Hello " + name + '!';
+  console.log('hello ' + name + '!')
 
 }
 
--- problems/8.types.js
+++ problems/8.types.js
@@ -17,7 +17,8 @@
  * @param {number} b - the second number
  * @returns {boolean} - if a and b are both number types, return true
  */
-function bothNumbers() {
+function bothNumbers(a, b) {
+  return a/1 === a && b/1 === b
 
 }
 
@@ -29,7 +30,9 @@ function bothNumbers() {
  * @returns {boolean} - return true if either a or b are a number
  * 
  */
-function eitherNumber() {
+function eitherNumber(a, b) {
+    return a/1 === a || b/1 === b
+
 
 }
 
@@ -40,7 +43,8 @@ function eitherNumber() {
  * @param {number} b - The second number to test
  * @returns {boolean} - If a and b are equal then return true, otherwise return false
  */
-function numbersEqual() {
+function numbersEqual(a, b) {
+    return a === b
 
 }
 
@@ -51,7 +55,8 @@ function numbersEqual() {
  * @param {number} b - The second number to test
  * @returns {boolean} - If a and b are not equal then return true, otherwise return false
  */
-function numbersNotEqual() {
+function numbersNotEqual(a, b) {
+    return a !== b
 
 }
 
@@ -63,8 +68,8 @@ function numbersNotEqual() {
  * @param {number} b - The second number to test
  * @returns {boolean} - If a and b are both even numbers, return true
  */
-function bothEven() {
-
+function bothEven(a, b) {
+  return a % 2 === 0 && b % 2 === 0
 }
 
 /**
@@ -74,8 +79,9 @@ function bothEven() {
  * @param {number} b - The second number to test
  * @returns {boolean} - If a and b are both odd numbers, return true
  */
-function bothOdd() {
-  
+function bothOdd(a, b) {
+    return a % 2 === 1 && b % 2 === 1
+
 }
 
 /**
@@ -85,7 +91,9 @@ function bothOdd() {
  * @param {number} b - The second number to test
  * @returns {boolean} - If either a or b is even, return true
  */
-function eitherEven() {
+function eitherEven(a, b) {
+    return a % 2 === 0 || b % 2 === 0
+    
 
 }
 
