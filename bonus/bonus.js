32410367002479af580f9b593d09d9ed7ca5a78e
Hafiz Ali <mohsantahir@yahoo.com>
2020-12-25 08:39:29 -0500

    second commit

--- bonus/bonus.js
+++ bonus/bonus.js
@@ -5,10 +5,14 @@
  * @param {numbers} this could be any number of parameters, all number type
  * @returns {number} - the sum of all the arguments passed in
  */
-function sumAll(This) {
-  Math.sumAll(this)
+function sumAll() {
+  let totalSum = 0
+  for (let i=0; i<arguments.length; i++) {
+    totalSum += arguments[i] 
+  }
+  return totalSum
 }
-sumAll
+console.log(sumAll(1,2,3,4,5,6))
 /**
  * Average all the arguments passed in, no matter how many there are
  * Hint: look up the `arguments` keyword. This will require you to loop.
@@ -17,8 +21,13 @@ sumAll
  * @returns {number} - A whole number the average of all the arguments passed in
  */
 function averageAll() {
-
+  let totalSum = 0
+  for (let x=0; x<arguments.length; x++) {
+    totalSum += arguments[x] 
+  }
+  return Math.round(totalSum/arguments.length)
 }
+console.log(averageAll(1,2,3,4,5))
 
 /**
  * Flip the sign of the number passed in.
