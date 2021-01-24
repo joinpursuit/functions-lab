62ff8257206a4f68a47e8c9a736dd165030037ca
Kobe <tahiriadam@gmail.com>
2021-01-24 12:14:57 -0500

    solved the bonus questions

--- bonus/bonus.js
+++ bonus/bonus.js
@@ -2,14 +2,16 @@
  * Sum all the arguments passed in, no matter how many there are
  * Hint: look up the `arguments` keyword. This will require you to loop.
  *
- * @param {numbers} this could be any number of parameters, all number type
+ * @param {numbers}  //could be any number of parameters, all number type
  * @returns {number} - the sum of all the arguments passed in
  */
-function sumAll(this) {
-  return arguments(this)
-
+function sumAll(numbers) {
+  let sum = 0;
+  for(let i = 0; i < arguments.length; i++) {
+    sum += arguments[i]
+  }
+  return sum;
 }
-
 /**
  * Average all the arguments passed in, no matter how many there are
  * Hint: look up the `arguments` keyword. This will require you to loop.
@@ -17,8 +19,12 @@ function sumAll(this) {
  * @param {numbers} this could be any number of parameters, all number type
  * @returns {number} - A whole number the average of all the arguments passed in
  */
-function averageAll() {
-
+function averageAll(numbers) {
+  let sum = 0;
+  for(let i = 0; i < arguments.length; i++) {
+    sum += arguments[i]
+  }
+  return Math.round((sum / arguments.length))
 }
 
 /**
@@ -30,7 +36,8 @@ function averageAll() {
  * @param {number} n - the number to be flipped.
  * @returns {number} - The opposite value of n
  */
-function flipSign() {
+function flipSign(n) {
+  return n * -1
 
 }
 
